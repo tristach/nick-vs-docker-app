495a4622835c70d928946626b2def96cddd8760b
Nick Stach <tristach@yahoo.com>
2026-06-13 16:00:07 +0900

    Added Flask web app with multiple routes.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
+RUN pip install flask
+
 COPY app.py .
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-print("Nick VS Docker")
-print("Learning Docker fundamentals")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/resume")
+def resume():
+    return "<h1>Future SOC Analyst</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Nick</h1>"
+
+app.run(host="0.0.0.0", port=5000)
